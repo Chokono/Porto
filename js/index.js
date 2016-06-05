8603050c269e15f9e404544a69bffce330a4e917
Da99er <Da99er87@ya.ru>
2016-06-05 14:02:30 +0300

    images

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-[].forEach.call(document.querySelectorAll('.steklo'),(e)=>{
-var t = [Math.floor(Math.random()*220),Math.floor(Math.random()*220),Math.floor(Math.random()*220)];
-e.style.backgroundColor=`rgba(${t[0]},${t[1]},${t[2]},0.30)`;
+[].forEach.call(document.querySelectorAll('.steklo'), (e) => {
+    var t = [Math.floor(Math.random() * 220), Math.floor(Math.random() * 220), Math.floor(Math.random() * 220)];
+    e.style.backgroundColor = `rgba(${t[0]},${t[1]},${t[2]},0.30)`;
 });
 
-BG.addEventListener('click',function(){if (BG.style.background==="none"){BG.style.background="url('../land.png') no-repeat"}
-	else {BG.style.background="none"};});
+BG.addEventListener('click', function() {
+    console.log("Bg", BG.id, BG.style);
+    if (BG.style.backgroundImage === "") {
+        BG.style.backgroundImage = "url('../land.png')";
+    } else {
+        BG.style.backgroundImage = "";
+    };
+});
