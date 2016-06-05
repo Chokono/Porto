a55bd6f072c554084f95ceecb2485474ef9daa43
Chokono <Andrew_Strubitskiy@mail.ru>
2016-06-05 13:34:02 +0400

    error

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@
 var t = [Math.floor(Math.random()*220),Math.floor(Math.random()*220),Math.floor(Math.random()*220)];
 e.style.backgroundColor=`rgba(${t[0]},${t[1]},${t[2]},0.30)`;
 });
+
+BG.addEventListener('click',function(){if (BG.style.background==="none"){BG.style.background="url('../land.png') no-repeat"}
+	else {BG.style.background="none"};});
